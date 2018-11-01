b64b066ceea925eac263561610cca74dd5caeebc
jkranjc <44633833+jkranjc@users.noreply.github.com>
2018-11-01 07:32:21 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -15,34 +15,14 @@ class MyClass(xbmcgui.Window):
 
   def localinfos(self):
     myinfos1 = xbmc.getLanguage()
-    self.strActionInfo = xbmcgui.ControlLabel(100, 150, 200, 200, '', 'font13', '0xFFFFFFFF')
+    self.strActionInfo = xbmcgui.ControlLabel(300, 150, 200, 200, '', 'font13', '0xFFFFFFFF')
     self.addControl(self.strActionInfo)
     self.strActionInfo.setLabel('Your language is : ' + myinfos1)
     myinfos2 = xbmc.getIPAddress()
-    self.strActionInfo = xbmcgui.ControlLabel(100, 200, 200, 200, '', 'font13', '0xFFFFFFFF')
+    self.strActionInfo = xbmcgui.ControlLabel(100, 200, 200, 200, '', 'font16', '0xFFFF2211')
     self.addControl(self.strActionInfo)
-    self.strActionInfo.setLabel('Your IP adress is : ' + myinfos2)
-    myinfos3 = xbmc.getDVDState()
-    self.strActionInfo = xbmcgui.ControlLabel(100, 250, 200, 200, '', 'font13', '0xFFFFFFFF')
-    self.addControl(self.strActionInfo)
-    dvdstate = ''
-    if (myinfos3 == 1):
-      dvdstate = 'DRIVE_NOT_READY'
-    if (myinfos3 == 16):
-      dvdstate = 'TRAY_OPEN'
-    if (myinfos3 == 64):
-      dvdstate = 'TRAY_CLOSED_NO_MEDIA'
-    if (myinfos3 == 96):
-      dvdstate = 'TRAY_CLOSED_MEDIA_PRESENT'            
-    self.strActionInfo.setLabel('dvd state : ' + dvdstate )
-    myinfos4 = xbmc.getFreeMem()
-    self.strActionInfo = xbmcgui.ControlLabel(100, 300, 200, 200, '', 'font13', '0xFFFFFFFF')
-    self.addControl(self.strActionInfo)
-    self.strActionInfo.setLabel('free mem : ' + str(myinfos4) + ' Mb')
-    myinfos5 = xbmc.getFreeMem()
-    self.strActionInfo = xbmcgui.ControlLabel(100, 350, 200, 200, '', 'font13', '0xFFFFFFFF')
-    self.addControl(self.strActionInfo)
-    self.strActionInfo.setLabel('cpu temp : ' + str(myinfos5) )
+	self.strActionInfo.setLabel('Your IP adress is : ' + myinfos2)
+
 
 mydisplay = MyClass()
 mydisplay.localinfos()
