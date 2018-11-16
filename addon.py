f6397a5a8746fc833c9e5a504873037b7f40c2a1
jkranjc <44633833+jkranjc@users.noreply.github.com>
2018-11-16 18:47:00 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -1,6 +1,6 @@
 from BeautifulSoup import BeautifulSoup
-import xbmc, xbmcgui
-import urllib2
+import xbmc, xbmcgui, xbmcaddon
+import urllib2, urllib, os
 
 #get actioncodes from https://github.com/xbmc/xbmc/blob/master/xbmc/guilib/Key.h
 ACTION_PREVIOUS_MENU = 10
@@ -33,15 +33,25 @@ class MyClass(xbmcgui.Window):
       self.addControl(self.strActionInfo)
       self.strActionInfo.setLabel(links1[1].text)
       vrsta = vrsta + 28
-    self.image = xbmcgui.ControlImage(50, 390, 480, 360, 'http://www.poglej.info/dorfarje.jpg')
+    myFile1 = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'slika1.jpg')
+    myFile2 = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'slika2.jpg')
+    myFile3 = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'slika3.jpg')
+    myFile4 = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'slika4.jpg')
+    myFile5 = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'slika5.jpg')
+    urllib.urlretrieve("http://www.poglej.info/dorfarje.jpg", myFile1)
+    urllib.urlretrieve("http://dorfarje.poglej.info/daytempdew.png", myFile2)
+    urllib.urlretrieve("http://dorfarje.poglej.info/daybarometer.png", myFile3)
+    urllib.urlretrieve("http://dorfarje.poglej.info/daywind.png", myFile4)
+    urllib.urlretrieve("http://dorfarje.poglej.info/daywinddir.png", myFile5)
+    self.image = xbmcgui.ControlImage(50, 390, 480, 360, myFile1)
     self.addControl(self.image)
-    self.image = xbmcgui.ControlImage(550, 100, 300, 180, 'http://dorfarje.poglej.info/daytempdew.png')
+    self.image = xbmcgui.ControlImage(550, 100, 300, 180, myFile2)
     self.addControl(self.image)
-    self.image = xbmcgui.ControlImage(870, 100, 300, 180, 'http://dorfarje.poglej.info/daybarometer.png')
+    self.image = xbmcgui.ControlImage(870, 100, 300, 180, myFile3)
     self.addControl(self.image)
-    self.image = xbmcgui.ControlImage(550, 300, 300, 180, 'http://dorfarje.poglej.info/daywind.png')
+    self.image = xbmcgui.ControlImage(550, 300, 300, 180, myFile4)
     self.addControl(self.image)
-    self.image = xbmcgui.ControlImage(870, 300, 300, 180, 'http://dorfarje.poglej.info/daywinddir.png')
+    self.image = xbmcgui.ControlImage(870, 300, 300, 180, myFile5)
     self.addControl(self.image)
 
 mydisplay = MyClass()
